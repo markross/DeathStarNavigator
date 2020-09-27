d13b8cabffdaacbb1dafa26f454a9538529b09ad
Mark Ross <mark@mross.co.uk>
2020-09-27 20:49:50 +0100

    Group moves in Path class by X position. Create addMove method.
    This allows new moves to be easily be inserted at a specific x pos.

--- spec/DeathStarNavigator/PathSpec.php
+++ spec/DeathStarNavigator/PathSpec.php
@@ -2,18 +2,41 @@
 
 namespace spec\DeathStarNavigator;
 
-use DeathStarNavigator\Path;
 use PhpSpec\ObjectBehavior;
 
 class PathSpec extends ObjectBehavior
 {
     function let()
     {
-        $this->beConstructedWith(['f','f','l','r']);
+        $this->beConstructedWith(['f','f','l','f','f','r']);
     }
 
     function it_stores_a_list_of_moves()
     {
-        $this->getMoves()->shouldBe(['f','f','l','r']);
+        $expected = [
+            0 => ['f'],
+            1 => ['f','l'],
+            2 => ['f'],
+            3 => ['f', 'r'],
+        ];
+        $this->getMoves()->shouldBeLike($expected);
     }
+
+    function it_can_insert_a_move_at_specified_position()
+    {
+        $this->addMove(['l','l'], 2);
+        $expected = [
+            0 => ['f', 'l', 'l'],
+            1 => ['f','l'],
+            2 => ['f'],
+            3 => ['f', 'r'],
+        ];
+        $this->getMoves()->shouldBeLike($expected);
+    }
+
+    function it_can_be_cast_to_a_string()
+    {
+        $this->__toString()->shouldBe('fflffr');
+    }
+
 }
--- src/DeathStarNavigator/Path.php
+++ src/DeathStarNavigator/Path.php
@@ -4,15 +4,63 @@ namespace DeathStarNavigator;
 
 class Path
 {
+    const DIRECTION_LEFT = 'l';
+    const DIRECTION_RIGHT = 'r';
+    const DIRECTION_FORWARD = 'f';
+
     private array $moves;
 
+    /**
+     *
+     * Path Constructor - takes an array of moves and groups them by X position. Left or Right moves maintain the
+     * x position. For example:
+     *
+     * 0 => 'f',
+     * 1 => 'f',
+     * 2 => 'f','l',
+     * 3 => 'f'
+     * 4 => 'f','r','r'
+     * 5 => 'f'
+     *
+     * @param array $moves
+     */
     public function __construct(array $moves)
     {
-        $this->moves = $moves;
+        $movesGroupedByXPos = [];
+        foreach ($moves as $move) {
+            if ($move === self::DIRECTION_FORWARD) {
+                $movesGroupedByXPos[] = [$move];
+            } else {
+                $movesGroupedByXPos[count($movesGroupedByXPos) - 1 ][] = $move;
+            }
+        }
+
+        $this->moves = $movesGroupedByXPos;
     }
 
-    public function getMoves()
+    /**
+     * @return array
+     */
+    public function getMoves() : array
     {
         return $this->moves;
+    }
+
+    /**
+     * Add a left or right move to the move list
+     *
+     * @param array $moves
+     * @param int $position
+     */
+    public function addMove(array $moves, int $position)
+    {
+        // check for left of right moves only
+        // @TODO - handle forward?
+        $this->moves[$position - 2] = [...$this->moves[$position - 2], ...$moves];
+    }
+
+    public function __toString() : string
+    {
+        return implode('', array_reduce($this->moves, 'array_merge', []));
     }
 }
