1c484249383f88c3484c5f1764449c8a4780bfc9
Mark Ross <mark@mross.co.uk>
2020-09-27 23:04:46 +0100

    Tidying up

--- run.php
+++ run.php
@@ -9,7 +9,7 @@ $config = Config::get();
 $pathFinder = new PathFinder(new DroidFactory(), $config->getTunnelLength());
 try {
     $path = $pathFinder->run();
-    echo "\n\nPath found at {$path}";
+    echo "\n\nPath found at {$path}\n";
 } catch (Exception $e) {
     echo "Something went wrong. Error was: \n\n{$e->getMessage()}\n";
 }
